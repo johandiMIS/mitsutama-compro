4fd99105593731a40d72e4d6ece9f93f31eaf043
Johandi <limjohandi@gmail.com>
2026-08-04 00:18:45 +0700

    header done

---
+++ apps/web/src/components/icons/ArrowRightToSquareIcon.tsx
@@ -0,0 +1,6 @@
+import { ArrowRightToSquare } from "@gravity-ui/icons";
+import type { IconProps } from "./icon-props";
+
+export function ArrowRightToSquareIcon({ size = 16, color = "currentColor" }: IconProps) {
+  return <ArrowRightToSquare width={size} height={size} style={{ color }} />;
+}
